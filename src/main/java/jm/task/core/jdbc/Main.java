beda96267eb249be24a37a551a97cdbee2fce74f
AndreyGorbachov <116598947+Andryhin@users.noreply.github.com>
2023-02-22 20:02:25 +0300

    comit

--- src/main/java/jm/task/core/jdbc/Main.java
+++
@@ -1,7 +0,0 @@
-package jm.task.core.jdbc;
-
-public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
-    }
-}
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++
@@ -1,5 +0,0 @@
-package jm.task.core.jdbc.util;
-
-public class Util {
-    // реализуйте настройку соеденения с БД
-}
